1df80fdd851d4610004dfc1ab60156de9602da2b
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-14 13:51:10 +0530

    fix: path for windows unit tests

--- setup-local/test/binaryControl.test.js
+++ setup-local/test/binaryControl.test.js
@@ -39,22 +39,22 @@ describe('Binary Control Operations', () => {
     const platformAndBinary = [
       {
         binary: BINARY_LINKS.DARWIN,
-        folder: `/work/binary/${LOCAL_BINARY_FOLDER}/darwin`,
+        folder: ['/work', 'binary', LOCAL_BINARY_FOLDER, 'darwin'],
         arch: 'x64',
         platform: PLATFORMS.DARWIN,
       }, {
         binary: BINARY_LINKS.LINUX_32,
-        folder: `/work/binary/${LOCAL_BINARY_FOLDER}/linux`,
+        folder: ['/work', 'binary', LOCAL_BINARY_FOLDER, 'linux'],
         arch: 'x32',
         platform: PLATFORMS.LINUX,
       }, {
         binary: BINARY_LINKS.LINUX_64,
-        folder: `/work/binary/${LOCAL_BINARY_FOLDER}/linux`,
+        folder: ['/work', 'binary', LOCAL_BINARY_FOLDER, 'linux'],
         arch: 'x64',
         platform: PLATFORMS.LINUX,
       }, {
         binary: BINARY_LINKS.WINDOWS,
-        folder: `/work/binary/${LOCAL_BINARY_FOLDER}/win32`,
+        folder: ['/work', 'binary', LOCAL_BINARY_FOLDER, 'win32'],
         arch: 'x32',
         platform: PLATFORMS.WIN32,
       },
@@ -66,7 +66,7 @@ describe('Binary Control Operations', () => {
         sinon.stub(os, 'arch').returns(system.arch);
         const binaryControl = new BinaryControl();
         expect(binaryControl.binaryLink).to.eql(system.binary);
-        expect(binaryControl.binaryFolder).to.include(path.resolve(system.folder));
+        expect(binaryControl.binaryFolder).to.include(path.resolve(...system.folder));
         os.platform.restore();
         os.arch.restore();
       });
