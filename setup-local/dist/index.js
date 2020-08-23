707c88a438de979c9c4c7960265b7451e76a78b9
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-23 19:48:55 +0530

    debug: linux binary

--- setup-local/dist/index.js
+++ setup-local/dist/index.js
@@ -1237,6 +1237,7 @@ class BinaryControl {
 
       core.info('Downloading BrowserStackLocal binary...');
       const downloadPath = await tc.downloadTool(this.binaryLink, path.resolve(this.binaryFolder, 'binaryZip'));
+      core.info(`Downloaded Path: ${downloadPath}`);
       // const extractedPath = await tc.extractZip(downloadPath, this.binaryFolder);
       // core.info(`BrowserStackLocal binary downloaded & extracted successfuly at: ${extractedPath}`);
       // const cachedPath = await tc.cacheDir(extractedPath, LOCAL_BINARY_NAME, '1.0.0');
