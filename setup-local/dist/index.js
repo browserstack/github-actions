5de124bc5e23f5669f0f29f564dc45d179d3756b
Rohan Chougule <rohan183chougule@gmail.com>
2020-08-23 17:47:18 +0530

    fix: binary deletion with extension for windows

--- setup-local/dist/index.js
+++ setup-local/dist/index.js
@@ -1225,7 +1225,10 @@ class BinaryControl {
       await this._makeDirectory();
       core.info('BrowserStackLocal binary not found in cache. Deleting any stale/existing binary from the download path...');
       const binaryZip = path.resolve(this.binaryFolder, 'binaryZip');
-      const previousLocalBinary = path.resolve(this.binaryFolder, LOCAL_BINARY_NAME);
+      const previousLocalBinary = path.resolve(
+        this.binaryFolder,
+        `${LOCAL_BINARY_NAME}${this.platform === PLATFORMS.WIN32 ? '.exe' : ''}`
+      );
       await Promise.all([io.rmRF(binaryZip), io.rmRF(previousLocalBinary)]);
 
       core.info('Downloading BrowserStackLocal binary...');
