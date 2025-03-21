5f2b20c456dbb44690e83922a97cf3cba96c4965
Aaryan <aaryan@browserstack.com>
2025-03-21 14:01:41 +0530

    fix new methods for actions/artifact

--- setup-local/src/artifactsManager.js
+++ setup-local/src/artifactsManager.js
@@ -1,4 +1,4 @@
-const artifact = require('@actions/artifact');
+const { DefaultArtifactClient } = require('@actions/artifact');
 const core = require('@actions/core');
 
 class ArtifactsManager {
@@ -7,16 +7,16 @@ class ArtifactsManager {
    * @param {String} artifactName Name by which the artifact should be available post uploading
    * @param {String[]} files Files to upload
    * @param {String} rootFolder Folder in which the files reside
-   * @returns {Promise<artifact.UploadResponse>} Response of the upload operation
+   * @returns {Promise<import('@actions/artifact').UploadArtifactResponse>} =>
+   * Response of the upload operation
    */
   static async uploadArtifacts(artifactName, files, rootFolder) {
-    const artifactClient = artifact.createArtifactClient();
+    const artifactClient = new DefaultArtifactClient();
     const response = await artifactClient.uploadArtifact(
       artifactName,
       files,
-      rootFolder, {
-        continueOnError: true,
-      },
+      rootFolder,
+      { continueOnError: true },
     );
     core.info(`Response for upload: ${JSON.stringify(response)}`);
     return response;
