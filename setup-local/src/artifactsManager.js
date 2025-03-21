74279a377db127aadfc86f8c8aef42ee022e14da
Aaryan <aaryan@browserstack.com>
2025-03-21 17:39:06 +0530

    fixted linting

--- setup-local/src/artifactsManager.js
+++ setup-local/src/artifactsManager.js
@@ -7,7 +7,7 @@ class ArtifactsManager {
    * @param {String} artifactName Name by which the artifact should be available post uploading
    * @param {String[]} files Files to upload
    * @param {String} rootFolder Folder in which the files reside
-   * @returns {Promise<import('@actions/artifact').UploadArtifactResponse>} =>
+   * @returns {Promise<import('@actions/artifact').UploadArtifactResponse>}
    * Response of the upload operation
    */
   static async uploadArtifacts(artifactName, files, rootFolder) {
@@ -15,8 +15,9 @@ class ArtifactsManager {
     const response = await artifactClient.uploadArtifact(
       artifactName,
       files,
-      rootFolder,
-      { continueOnError: true },
+      rootFolder, {
+        continueOnError: true,
+      },
     );
     core.info(`Response for upload: ${JSON.stringify(response)}`);
     return response;
